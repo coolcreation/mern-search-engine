87cdb296b0be927e5da1a250057e17a422743666
Jeff <jeffbohn@my.cwi.edu>
2025-06-07 14:33:55 -0600

    add apiKey .env variable name in meiliClient.js

--- backend/meiliClient.js
+++ backend/meiliClient.js
@@ -7,7 +7,7 @@ const client = new MeiliSearch({
     // host: 'http://localhost:7700', // For local development
     host: 'https://collab-ecommerce-meili-proxy.onrender.com',
     //host: process.env.MEILI_HOST, // Meilisearch hosted on Render
-    //apiKey: process.env.MEILI_MASTER_KEY,  // Replace with your master key or an API key
+    apiKey: process.env.MEILI_MASTER_KEY,  // Replace with your master key or an API key
 });
 console.log(client);
 
