7e6a2e8dcaeecde9249a4e3b4d1369707f4863ef
Jeff <jeffbohn@my.cwi.edu>
2025-06-07 13:29:50 -0600

    remove  apiKey .env variable name in meiliClient.js

--- backend/meiliClient.js
+++ backend/meiliClient.js
@@ -7,7 +7,23 @@ const client = new MeiliSearch({
     // host: 'http://localhost:7700', // For local development
     host: 'https://collab-ecommerce-meili-proxy.onrender.com',
     //host: process.env.MEILI_HOST, // Meilisearch hosted on Render
-    apiKey: process.env.MEILI_MASTER_KEY,  // Replace with your master key or an API key
+    //apiKey: process.env.MEILI_MASTER_KEY,  // Replace with your master key or an API key
 });
 console.log(client);
+
+async function testConnection() {
+  try {
+    const indexes = await client.getIndexes()
+    console.log('✅ Connected. Indexes:', indexes)
+  } catch (err) {
+    console.error('❌ Failed to connect:', err.message)
+  }
+}
+
+testConnection()
+
 export default client;
+
+
+
+
